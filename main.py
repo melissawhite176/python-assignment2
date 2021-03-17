484085df6fa6b85e2d6a0de9d953c337cdaf0025
Melissa White <melissawhite842@gmail.com>
2021-03-17 20:43:45 +0000

    completed assignment

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ print(10 == "10")
 # 9. What is the result of “bag” > “apple”? True
 print("bag" > "apple")
 # 10. What is the result of not(True or False)?
-print(not(True or False))
+print('not(True or False):', not(True or False))
 print(not(True))
 print(not(False))
 # 11. Under what circumstances does the expression 18 <= age < 65 evaluate to True? Age between 18 and 65 (18 inclusive)
@@ -131,43 +131,133 @@ print(list(range(1, 10, 2)))
 
 # Functions
 # 1. What is the difference between a parameter and an argument?
-# 2. All functions in Python by default return ...?
+  # A parameter is the variable listed inside the parentheses in the function definition.
+  # An argument is the value that is sent to the function when it is called. (https://www.w3schools.com/python/gloss_python_function_arguments.asp)
+# 2. All functions in Python by default return ...? None
 # 3. What are keyword arguments and when should we use them?
+  #Sending arguments with key-value pairs
+def myPets(cat, dog, fish):
+  print('My dog\'s name is', dog)
+myPets(cat='Billy', dog='Bob', fish='Bonnie')
 # 4. How can we make a parameter of a function optional?
+  # By setting a default value to an argument, using the *args parameter
 # 5. What happens when we prefix a parameter with an asterisk (*)?
+  # *args - makes the parameter optional
+def myPets2(cat, dog, *fish):
+  print('My pets\' names are:', cat, ',', dog, ',', *fish)
+myPets2('Billy', 'Bob')
+myPets2('Billy', 'Bob', 'Bonnie')
+
 # 6. What about two asterisks (**)?
+  # **kwargs - similar to *args, but accepts keyword arguments
+def myPets3(**pets):
+  for animal, name in pets.items():
+    print(animal, '=', name)
+myPets3(cat='Billy', dog='Bob', fish='Bonnie')
 # 7. What is scope?
+  #A variable is only available from inside the region it is created. This is called scope. (https://www.w3schools.com/python/python_scope.asp)
 # 8. What is the difference between local and global variables?
+  # local scope - A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+  # global scope - A variable created in the main body of the Python code is a global variable and belongs to the global scope. Global variables are available from within any scope, global and local. (https://www.w3schools.com/python/python_scope.asp)
+
 # 9. Why is using the global statement a bad practice?
+  # Global variables are dangerous because they can be simultaneously accessed from multiple sections of a program. This frequently results in bugs. Most bugs involving global variables arise from one function reading and acting on the value of a global variable before another function has the chance to set it to an appropriate value. Global variables also make code difficult to read, because they force you to search through multiple functions or even modules just to understand all the different locations where the global variable is used and modified. (https://docs.quantifiedcode. com/python-anti-patterns/maintainability/using_the_global_statement.html)
 
 # Coding Exercises
 # 1. Write a function that returns the maximum of two numbers.
+def twoNumsOnly(a, b):
+  print(a, b)
+  return a, b
+twoNumsOnly(3, 5)
 # 2. Write a function called fizz_buzz that takes a number.
-# 1. If the number is divisible by 3, it should return “Fizz”.
-# 2. If it is divisible by 5, it should return “Buzz”.
-# 3. If it is divisible by both 3 and 5, it should return “FizzBuzz”.
-# 4. Otherwise, it should return the same number.
-# 3. Write a function for checking the speed of drivers. This function should have one parameter:
-# speed.
-# 1. If speed is less than 70, it should print “Ok”.
-# 2. Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit
-# point and print the total number of demerit points. For example, if the speed is 80, it
-# should print: “Points: 2”.
-# 3. If the driver gets more than 12 points, the function should print: “License suspended”
-# 4. Write a function called showNumbers that takes a parameter called limit. It should print all
-# the numbers between 0 and limit with a label to identify the even and odd numbers. For
-# example, if the limit is 3, it should print:
-# o 0 EVEN
-# o 1 ODD
-# o 2 EVEN
-# o 3 ODD
-# 5. Write a function that returns the sum of multiples of 3 and 5 between 0 and limit (parameter).
-# For example, if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, 20.
+  # If the number is divisible by 3, it should return “Fizz”.
+  # If it is divisible by 5, it should return “Buzz”.
+  # If it is divisible by both 3 and 5, it should return “FizzBuzz”
+  # Otherwise, it should return the same number.
+def fizz_buzz(num):
+  if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+  elif num % 3 == 0:
+    print("Fizz")
+  elif num % 5 == 0:
+    print("Buzz")
+  else:
+    print(num)
+  
+fizz_buzz(5)
+fizz_buzz(3)
+fizz_buzz(15)
+fizz_buzz(1)
+fizz_buzz(2)
+
+
+# 3. Write a function for checking the speed of drivers. This function should have one parameter: speed.
+  # If speed is less than 70, it should print “Ok”.
+  # Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points. For example, if the speed is 80, it should print: “Points: 2”.
+  # If the driver gets more than 12 points, the function should print: “License suspended”
+def checkSpeed(speed):
+  if speed < 70:
+    print("OK")
+  elif speed >= 75:
+    overLimit = speed - 70
+    points = int(overLimit / 5)
+    print('points:', points)
+    if points > 12:
+      print("License suspended")
+checkSpeed(200)
+# 4. Write a function called showNumbers that takes a parameter called limit. It should print all the numbers between 0 and limit with a label to identify the even and odd numbers. For example, if the limit is 3, it should print:
+  # o 0 EVEN
+  # o 1 ODD
+  # o 2 EVEN
+  # o 3 ODD
+
+def showNumbers(limit):
+  numList = list(range(0, limit, 1))
+  for number in numList:
+    if number % 2 == 0:
+      print(number, 'EVEN')
+    else:
+      print(number, 'ODD')
+
+showNumbers(10)
+
+# 5. Write a function that returns the sum of multiples of 3 and 5 between 0 and limit (parameter). For example, if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, 20.
+print('sumMultiples()')
+def sumMultiples(limit):
+  numList = list(range(0, limit, 1))
+  sums = 0
+  for number in numList:
+    if number % 3 == 0:
+      print('multiple of 3:', number)
+      sums += number
+    if number % 5 == 0:
+      print('multiple of 5:', number)
+      sums += number
+  print('Sum of Multiples:', sums)
+
+sumMultiples(20)
+
 # 6. Write a function called show_stars(rows). If rows is 5, it should print the following:
-# o *
-# o **
-# o ***
-# o ****
-# o *****
-# 7. Write a function that prints all the prime numbers between 0 and limit where limit is a
-# parameter.
+  # o *
+  # o **
+  # o ***
+  # o ****
+  # o *****
+def show_stars(rows):
+  rowList = list(range(1, rows + 1, 1))
+  for row in rowList:
+    print('*' * row)
+show_stars(5)
+# 7. Write a function that prints all the prime numbers between 0 and limit where limit is a parameter
+
+def printPrimeNums(limit):
+  for num in range(0, limit + 1):
+     # all prime numbers are greater than 1
+     if num > 1:
+         for i in range(2, num):
+             if (num % i) == 0:
+                 break
+         else:
+             print(num)
+
+printPrimeNums(30)
